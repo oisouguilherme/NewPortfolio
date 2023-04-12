b0031147d97531bd82a1db66a363cf7e0b6ce068
Guilherme Henrique <gh.dourado77@gmail.com>
2023-04-12 15:31:41 -0300

    ajustando border

--- src/components/Conhecimentos/CardConhecimento/index.jsx
+++ src/components/Conhecimentos/CardConhecimento/index.jsx
@@ -1,6 +1,6 @@
 export function CardConhecimento({image, text}){
   return(
-    <div className="bg-black flex items-center gap-6 px-6 py-2 w-72 text-white font-medium cursor-pointer hover:border-solid hover:border-2 hover:border-purple-800">
+    <div className="bg-black flex items-center gap-6 px-6 py-2 w-72 text-white font-medium cursor-pointer hover:border-solid border-black border-2 hover:border-purple-800">
       <img src={image} alt={text} />
       <p>{text}</p>
     </div>
