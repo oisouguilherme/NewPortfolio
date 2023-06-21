cd83f96abd0fa9aaed3397936efae82bc9a4f288
Guilherme Henrique <gh.dourado77@gmail.com>
2023-06-21 13:06:43 -0300

    atualizando com novos projetos

--- src/components/Projetos/Slide/index.jsx
+++ src/components/Projetos/Slide/index.jsx
@@ -1,16 +1,16 @@
 export function Slide({projeto,linkGithub,linkSite,imagem}) {
   return (
-    <div className="w-full h-72 relative ">
+    <div className="w-full h-96 relative ">
       <div className="absolute bottom-6 left-6">
         <div className="space-x-3">
           <a
-            className="bg-purple-950 hover:bg-purple-800 px-6 py-1 rounded text-white font-medium text-lg duration-500 cursor-pointer"
+            className="bg-purple-900 hover:bg-purple-800 px-6 py-1 rounded text-white font-medium text-lg duration-500 cursor-pointer"
             href={linkGithub} target="_blank"
           >
             Github
           </a>
           <a
-            className="bg-purple-950 hover:bg-purple-800 px-6 py-1 rounded text-white font-medium text-lg duration-500 hover:scale-110 cursor-pointer"
+            className="bg-purple-900 hover:bg-purple-800 px-6 py-1 rounded text-white font-medium text-lg duration-500 hover:scale-110 cursor-pointer"
             href={linkSite} target="_blank"
           >
             Site
--- src/components/Projetos/index.jsx
+++ src/components/Projetos/index.jsx
@@ -1,16 +1,17 @@
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
-import ImagemTeste from "../../assets/imagemteste.png";
+
 import { Slide } from "./Slide";
 import { Botao } from "./Botao";
 
-import projeto1 from '../../assets/projetos/1.png'
-import projeto2 from '../../assets/projetos/2.png'
-import projeto3 from '../../assets/projetos/3.png'
-import projeto4 from '../../assets/projetos/4.png'
-import projeto5 from '../../assets/projetos/5.png'
-import projeto6 from '../../assets/projetos/6.png'
-import projeto7 from '../../assets/projetos/7.png'
+import projeto1 from "../../assets/projetos/1.png";
+import projeto2 from "../../assets/projetos/2.png";
+import projeto3 from "../../assets/projetos/3.png";
+import projeto4 from "../../assets/projetos/4.png";
+import projeto8 from "../../assets/projetos/projeto8.png";
+import projeto9 from "../../assets/projetos/projeto9.png";
+import projeto10 from "../../assets/projetos/projeto10.png";
+import projeto11 from "../../assets/projetos/projeto11.png";
 
 export function Projetos() {
   return (
@@ -31,29 +32,69 @@ export function Projetos() {
         onSwiper={(swiper) => console.log(swiper)}
       >
         <SwiperSlide>
-          <Slide linkGithub={"https://github.com/oisouguilherme/GOD-OF-WAR-Ladind-Page"} linkSite={"https://god-of-war-e784d9.netlify.app/"} imagem={projeto1} />
+          <Slide
+            linkGithub={
+              "https://github.com/oisouguilherme/GOD-OF-WAR-Ladind-Page"
+            }
+            linkSite={"https://god-of-war-e784d9.netlify.app/"}
+            imagem={projeto1}
+          />
         </SwiperSlide>
         <SwiperSlide>
-          <Slide linkGithub={"https://github.com/oisouguilherme/ConsultoriaUlisses"} linkSite={"https://ulissesalmeida.com.br/"} imagem={projeto2} />
+          <Slide
+            linkGithub={"https://github.com/oisouguilherme/ConsultoriaUlisses"}
+            linkSite={"https://ulissesalmeida.com.br/"}
+            imagem={projeto2}
+          />
         </SwiperSlide>
         <SwiperSlide>
-          <Slide linkGithub={"https://github.com/oisouguilherme/PaginaHotelTailwind"} linkSite={"https://home-hotel-04decf.netlify.app/"} imagem={projeto3} />
+          <Slide
+            linkGithub={"https://github.com/oisouguilherme/PaginaHotelTailwind"}
+            linkSite={"https://home-hotel-04decf.netlify.app/"}
+            imagem={projeto3}
+          />
         </SwiperSlide>
         <SwiperSlide>
-          <Slide linkGithub={"https://github.com/oisouguilherme/Freelancer-projeto1"} linkSite={"https://curso-dobre-suas-vendas.ulissesalmeida.com.br/"} imagem={projeto4} />
+          <Slide
+            linkGithub={"https://github.com/oisouguilherme/Freelancer-projeto1"}
+            linkSite={"https://curso-dobre-suas-vendas.ulissesalmeida.com.br/"}
+            imagem={projeto4}
+          />
         </SwiperSlide>
         <SwiperSlide>
-          <Slide linkGithub={"https://github.com/oisouguilherme/Freelancer_projeto2"} linkSite={"https://dobre-suas-vendas-ebook.ulissesalmeida.com.br/"} imagem={projeto5} />
+          <Slide
+            linkGithub={"https://github.com/oisouguilherme/RafaelPrudentePage"}
+            linkSite={"https://boisterous-sable-6b290d.netlify.app/"}
+            imagem={projeto8}
+          />
         </SwiperSlide>
         <SwiperSlide>
-          <Slide linkGithub={"https://github.com/oisouguilherme/Alura-Newsletter"} linkSite={"https://newsletter-alu.netlify.app/"} imagem={projeto6} />
+          <Slide
+            linkGithub={"https://github.com/oisouguilherme/CofenPisoSalarial"}
+            linkSite={"https://cofen-piso-salarial-page.vercel.app/"}
+            imagem={projeto9}
+          />
         </SwiperSlide>
         <SwiperSlide>
-          <Slide linkGithub={"https://github.com/oisouguilherme/organoReactAlura"} linkSite={"https://organo-react-alura-azure.vercel.app/"} imagem={projeto7}/>
+          <Slide
+            linkGithub={"https://github.com/oisouguilherme/PageCorretoraZunic"}
+            linkSite={"https://page-corretora-zunic.vercel.app/"}
+            imagem={projeto10}
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Slide
+            linkGithub={"https://github.com/oisouguilherme/sertaoConsultoria"}
+            linkSite={"https://inspiring-rolypoly-6d7c18.netlify.app/"}
+            imagem={projeto11}
+          />
         </SwiperSlide>
       </Swiper>
       <div className="flex justify-center">
-        <Botao text={"Abrir Gihthub"} link={"https://github.com/oisouguilherme?tab=repositories"} />
+        <Botao
+          text={"Abrir Gihthub"}
+          link={"https://github.com/oisouguilherme?tab=repositories"}
+        />
       </div>
     </section>
   );
