915b5b1a9b83b4aabd9edc5e2791920fe5354a15
Guilherme Henrique <gh.dourado77@gmail.com>
2023-07-13 15:58:07 -0300

    atualizando sessão dos meus projetos

---
+++ src/components/Projetos/CardProjeto/index.jsx
@@ -0,0 +1,29 @@
+import Logo from "../../../assets/logo.png";
+export function CardProjeto({ image, title, client, logoClient, tec1, tec2 }) {
+  return (
+    <div className="text-white w-80 bg-white bg-opacity-5 rounded-xl overflow-hidden">
+      <div className="p-4">
+        <div className="flex justify-end gap-4 pb-4 text-sm">
+          <p className="bg-gray-300 px-4 py-1 rounded-full text-black font-medium">
+            {tec1}
+          </p>
+          <p className="bg-gray-300 px-4 py-1 rounded-full text-black font-medium">
+            {tec2}
+          </p>
+        </div>
+        <div className="flex items-center gap-2 text-sm">
+          <img src={Logo} alt={client} className="w-8" />
+          <p>{client}</p>
+        </div>
+        <h2 className="text-xl font-bold">{title}</h2>
+        <p className="text-sm">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam
+          amet labore recusandae enim, sed laboriosam.
+        </p>
+      </div>
+      <div>
+        <img src={image} alt={title} />
+      </div>
+    </div>
+  );
+}
--- src/components/Projetos/index.jsx
+++ src/components/Projetos/index.jsx
@@ -1,17 +1,13 @@
-import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/css";
-
-import { Slide } from "./Slide";
-import { Botao } from "./Botao";
-
-import projeto1 from "../../assets/projetos/1.png";
-import projeto2 from "../../assets/projetos/2.png";
-import projeto3 from "../../assets/projetos/3.png";
-import projeto4 from "../../assets/projetos/4.png";
-import projeto8 from "../../assets/projetos/projeto8.png";
-import projeto9 from "../../assets/projetos/projeto9.png";
-import projeto10 from "../../assets/projetos/projeto10.png";
-import projeto11 from "../../assets/projetos/projeto11.png";
+import { CardProjeto } from "./CardProjeto";
+import Page1 from '../../assets/projetos/page1.png'
+import Page2 from '../../assets/projetos/page2.png'
+import Page3 from '../../assets/projetos/page3.png'
+import Page4 from '../../assets/projetos/page4.png'
+import Page5 from '../../assets/projetos/page5.png'
+import Page6 from '../../assets/projetos/page6.png'
+import Page7 from '../../assets/projetos/page7.png'
+import Page8 from '../../assets/projetos/page8.png'
+import Page9 from '../../assets/projetos/page9.png'
 
 export function Projetos() {
   return (
@@ -19,81 +15,69 @@ export function Projetos() {
       <h2 className="text-white font-bold text-4xl text-center">
         Meus Projetos
       </h2>
-      <Swiper
-        breakpoints={{
-          620: {
-            width: 620,
-            slidesPerView: 2,
-          },
-        }}
-        spaceBetween={20}
-        slidesPerView={1}
-        onSlideChange={() => console.log("slide change")}
-        onSwiper={(swiper) => console.log(swiper)}
-      >
-        <SwiperSlide>
-          <Slide
-            linkGithub={
-              "https://github.com/oisouguilherme/GOD-OF-WAR-Ladind-Page"
-            }
-            linkSite={"https://god-of-war-e784d9.netlify.app/"}
-            imagem={projeto1}
-          />
-        </SwiperSlide>
-        <SwiperSlide>
-          <Slide
-            linkGithub={"https://github.com/oisouguilherme/ConsultoriaUlisses"}
-            linkSite={"https://ulissesalmeida.com.br/"}
-            imagem={projeto2}
-          />
-        </SwiperSlide>
-        <SwiperSlide>
-          <Slide
-            linkGithub={"https://github.com/oisouguilherme/PaginaHotelTailwind"}
-            linkSite={"https://home-hotel-04decf.netlify.app/"}
-            imagem={projeto3}
-          />
-        </SwiperSlide>
-        <SwiperSlide>
-          <Slide
-            linkGithub={"https://github.com/oisouguilherme/Freelancer-projeto1"}
-            linkSite={"https://curso-dobre-suas-vendas.ulissesalmeida.com.br/"}
-            imagem={projeto4}
-          />
-        </SwiperSlide>
-        <SwiperSlide>
-          <Slide
-            linkGithub={"https://github.com/oisouguilherme/RafaelPrudentePage"}
-            linkSite={"https://boisterous-sable-6b290d.netlify.app/"}
-            imagem={projeto8}
-          />
-        </SwiperSlide>
-        <SwiperSlide>
-          <Slide
-            linkGithub={"https://github.com/oisouguilherme/CofenPisoSalarial"}
-            linkSite={"https://cofen-piso-salarial-page.vercel.app/"}
-            imagem={projeto9}
-          />
-        </SwiperSlide>
-        <SwiperSlide>
-          <Slide
-            linkGithub={"https://github.com/oisouguilherme/PageCorretoraZunic"}
-            linkSite={"https://page-corretora-zunic.vercel.app/"}
-            imagem={projeto10}
-          />
-        </SwiperSlide>
-        <SwiperSlide>
-          <Slide
-            linkGithub={"https://github.com/oisouguilherme/sertaoConsultoria"}
-            linkSite={"https://inspiring-rolypoly-6d7c18.netlify.app/"}
-            imagem={projeto11}
-          />
-        </SwiperSlide>
-      </Swiper>
-      <div className="flex justify-center">
-        <Botao
-          text={"Abrir Gihthub"}
-          link={"https://github.com/oisouguilherme?tab=repositories"}
+      <div className="max-w-6xl mx-auto flex flex-wrap gap-12 justify-center">
+        <CardProjeto
+          title={"Consultoria Ambiental: "}
+          client={"Edvaldo - Consultoria"}
+          image={Page1}
+          tec1={"Next JS"}
+          tec2={"SPA"}
+        />
+        <CardProjeto
+          title={"Clinica Medica:"}
+          client={"Clinica Buritis"}
+          image={Page2}
+          tec1={"Next JS"}
+          tec2={"React-Scroll"}
+        />
+        <CardProjeto
+          title={"Contabilidade:"}
+          client={"Contabilidade Inova"}
+          image={Page3}
+          tec1={"Next JS"}
+          tec2={"TailwindCSS"}
+        />
+        <CardProjeto
+          title={"E-Comerce IPhone:"}
+          client={"GoiStore - Italo Gois"}
+          image={Page4}
+          tec1={"Next JS"}
+          tec2={"E-Comerce"}
+        />
+        <CardProjeto
+          title={"Corretora de Seguros:"}
+          client={"Zunic - Corretora de Seguros"}
+          image={Page5}
+          tec1={"Pagina Instiucional"}
+          tec2={"AOS"}
+        />
+        <CardProjeto
+          title={"Mentoria Instagram:"}
+          client={"Venda Qualquer coisa"}
+          image={Page6}
+          tec1={"React JS"}
+          tec2={"TailwindCSS"}
+        />
+        <CardProjeto
+          title={"Curso de Vendas:"}
+          client={"Venda Qualquer coisa"}
+          image={Page7}
+          tec1={"React JS"}
+          tec2={"HTML / CSS"}
+        />
+        <CardProjeto
+          title={"Rafael Prudente Institucional:"}
+          client={"Rafael Prudente - Governador"}
+          image={Page8}
+          tec1={"Next JS"}
+          tec2={"TailwindCSS"}
+        />
+        <CardProjeto
+          title={"Cofen Infermagem:"}
+          client={"Conselho federal de Infermagem"}
+          image={Page9}
+          tec1={"Next JS"}
+          tec2={"TailwindCSS"}
         />
       </div>
     </section>
